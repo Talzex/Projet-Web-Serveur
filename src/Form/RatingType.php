f0bb806959171bd1737434e2afcb4625257c3265
tduthil <thomas.duthil@etu.u-bordeaux.fr>
2021-12-30 12:58:13 +0100

    Espace Commentaire

--- src/Form/RatingType.php
+++ src/Form/RatingType.php
@@ -16,6 +16,12 @@ class RatingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+
+            ->add('comment', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => 'Ce film est vraiment chouette... Je vous conseille de le regarder!',
+                ]
+            ])
             ->add('value', NumberType::class, [
                 'label' => 'Note sur 10 :',
                 'attr' => [
@@ -24,16 +30,12 @@ class RatingType extends AbstractType
                     'type' => 'number'
                 ]
             ])
-            ->add('comment', TextareaType::class, [
-                'label' => 'Votre commentaire :',
-                'attr' => [
-                    'placeholder' => 'Ce film est vraiment chouette... Je vous conseille de le regarder!',
-                    ]
-            ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Envoyer'
-            ])
-        ;
+                'label' => 'Envoyer',
+                'attr' => [
+                    'class' => 'btn btn-outline-light my-4'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
