08fc4f8da34589b01baa2ea85d7c87986701cc22
Julian Forme <julian.forme@etu.u-bordeaux.fr>
2022-01-07 16:51:19 +0100

    update captcha

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
 
 class RegistrationController extends AbstractController
 {
